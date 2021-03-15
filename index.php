dea6fe9ffaf90afa50fce8bf8b42fd84f8fcdfa9
HSDKTECH <79018042+HSDKTECH@users.noreply.github.com>
2021-03-15 10:00:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
             <div class="row it_works">
               <?php
 
-                        $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb');
+                        $link = mysqli_connect('54.161.73.98', 'ecomuser', 'ecompassword', 'ecomdb');
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
